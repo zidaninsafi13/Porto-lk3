f9f446c9d5602fd4ba086e07f8dcf37a1ff2417e
zidaninsafi13 <zidaninsafi164@gmail.com>
2026-09-08 08:05:20 +0700

    add

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './',
+  base: '/Porto-lk3/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
